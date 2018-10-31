3ab64942e825cc26d34411647afb057bf58e5641
Scoefield <scoefield@163.com>
2018-10-31 18:21:53 +0800

    modify some filee

---
+++ Shop/shopping/urls.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     urls
+   Author :       hyt
+   Create date：          2018/10/31
+-------------------------------------------------
+"""
+from django.urls import path
+from . import views
+
+app_name = 'shopping'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<phone_id>/', views.single_web, name='single_web'),
+    path('<good_id>/', views.good_detail, name='good_detail')
+]
--- Shop/shopping/views.py
+++ Shop/shopping/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
+
 
 # Create your views here.
+def home(request):
+    # 所有商品类别
+    alltypes = models.ShopList.objects.all()
+    # 类型与商品
+    type_and_goods = []
+    for typer in alltypes:
+        type_and_goods.append((typer, models.ProdList.objects.filter(typer=typer, havup=True)))
+    content = {'type_and_goods': type_and_goods, 'alltypes': alltypes}
+    return render(request, 'shopping/home.html', content)
+
+
+def single_web(request, phone_id):
+    alltypes = models.ShopList.objects.all()
+    # 所属类别
+    blg_cgy = get_object_or_404(models.ShopList, id=phone_id)
+    # 类型与商品
+    type_and_goods = [(blg_cgy, models.ProdList.objects.filter(typer=blg_cgy, havup=True))]
+    content = {'type_and_goods': type_and_goods, 'alltypes': alltypes}
+    return render(request, 'shopping/home.html', content)
+
+
+def good_detail(request, phone_id, good_id):
+    # 根据商品ID获取商品详细信息
+    gd_detail = get_object_or_404(models.ProdList, id=good_id)
+    content = {'gd_detail': gd_detail}
+    return render(request, 'shopping/detail.html', content)
+
